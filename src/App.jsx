a77fa7101857dfab06b830df7bd6ddcbee1d37f0
Dani <dustyglass@gmail.com>
2024-09-15 18:36:14 -0300

    project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+  <main className='main'>
+   <div className='container'>
+      <h1 className='title'>¡Bienvenido a mi aplicación de React!</h1>
+      <h2 className='subtitle'>
+        Lista de cosas por hacer:
+      </h2>
+      <p className='text'>
+        Aprender React
+      </p>
+      <p className='text'>
+        Practicar con Vite
+      </p>
+      <p className='text'>
+        Construir proyectos increíbles
+      </p>
+    </div>
+  </main>
+   
+  )
+}
+
+export default App
